92f10bc08d6454b3dee5206aa82b6141f5a2da34
Viktoria Czaran <vczaran@gmail.com>
2023-08-05 16:33:57 -0400

    Complete backend user auth

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,10 +4,25 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET/api/users"
+  });
+});
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
   });
 });
 
@@ -42,7 +57,7 @@ router.post('/register', async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       }
       catch (err) {
         next(err);
@@ -61,7 +76,7 @@ router.post('/login', async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
 });
 
