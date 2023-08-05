d96a199b3fdeef4985a9fb74a285257825e1f552
Viktoria Czaran <vczaran@gmail.com>
2023-08-05 14:48:22 -0400

    Add CORS and CSRF protections

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { isProduction } = require('./config/keys');
+const cors = require('cors');
+const csurf = require('csurf');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -12,7 +16,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// CORS Security middleware - enable cors only in development (since server and react are on different ports)
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI
+    mongoURI: process.env.MONGO_URI,
+    isProductino: process.env.NODE_ENV === 'production'
   }
