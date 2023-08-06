72bd7df4ee824bf5227c2dd09d1545f602d0bf21
Viktoria Czaran <vczaran@gmail.com>
2023-08-05 20:07:53 -0400

    Working on react components

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>A MERN Twitter Clone</p>
+        <footer>
+          Copyright &copy; 2023 Chirper
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
